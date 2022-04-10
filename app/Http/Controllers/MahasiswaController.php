07e5a7056cb451893747f747917aa120969ca851
Kev9794 <k.natanael88@gmail.com>
2022-04-10 22:18:33 +0700

    Tugas 2 : Membuat Pagination

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -17,8 +17,9 @@ class MahasiswaController extends Controller
     {
         //fungsi eloquent menampilkan data menggunakan pagination
         $mahasiswa = $mahasiswa = DB::table('mahasiswa')->get(); // Mengambil semua isi tabel
-        $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(6);
-        return view('mahasiswa.index', compact('mahasiswa'))->with('i',(request()->input('page',1)-1)*5);
+        $posts = Mahasiswa::orderBy('Nim', 'desc')->paginate(3);
+        return view('mahasiswa.index', ['mahasiswa' => $posts]);
+        // return view('mahasiswa.index', compact('mahasiswa'))->with('i',(request()->input('page',1)-1)*5);
     }
     public function create()
     {
